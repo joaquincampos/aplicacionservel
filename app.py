698c8e8838350d8e20a93f1fec0ad44bf29f1014
PrincesoDan <daniel.andrade1991@gmail.com>
2023-11-16 18:57:32 -0300

    Actualizar directorio de datos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 st.title("Aplicación datos SERVEL")
-df = pd.read_parquet("datos_servel_2022.parquet")
+df = pd.read_parquet("./datos/datos_servel_2022.parquet")
 st.dataframe(df)
