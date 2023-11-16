ea503cd5ad395d147d1cea7f3f6ff594c64d6de3
PrincesoDan <daniel.andrade1991@gmail.com>
2023-11-16 19:21:31 -0300

    ingreso de datos de los colegios

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
+
 st.title("Aplicación datos SERVEL")
 df = pd.read_parquet("./datos/datos_servel_2022.parquet")
-st.dataframe(df)
+st.dataframe(df.head())
+colegios = gpd.read_file("./datos/Establecimientos Educacionales 2021/Establecimientos_Educacionales_2021.shp")
+st.write(colegios.head())
+
+
+    
